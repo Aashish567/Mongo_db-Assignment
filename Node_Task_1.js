a3feb0516ac8cd9292d01f2c2f8dd22042c3a7be
Aashish567 <59502875+Aashish567@users.noreply.github.com>
2023-08-13 22:00:51 +0530

    Add files via upload

---
+++ Node_Task_1.js
@@ -0,0 +1,17 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+
+  res.end('Hello, World!');
+});
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  document.write(`Server is running on port ${PORT}`);
+});
