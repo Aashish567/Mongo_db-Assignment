95b8cbe2ef6039e6fdcccb4b8934c832fa407eea
Aashish567 <59502875+Aashish567@users.noreply.github.com>
2023-08-13 22:13:39 +0530

    Add files via upload

---
+++ FS Module.js
@@ -0,0 +1,15 @@
+
+const fs = require('fs');
+
+fs.readFile('data.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+
+  console.log('Content of data.txt:');
+
+  console.log(data.toString());
+
+});
+
